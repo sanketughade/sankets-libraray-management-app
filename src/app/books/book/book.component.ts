961810173a66d9a6e4568495ce572748ab8cc3fe
sanketughade <sanketughade96@gmail.com>
2021-06-29 20:39:54 +0530

    Added an library empty danger message when all books in the library are deleted.

--- src/app/books/book/book.component.ts
+++ src/app/books/book/book.component.ts
@@ -10,14 +10,18 @@ import { BooksService } from '../books.service';
 export class BookComponent implements OnInit {
 books: Book[]=[];
 arrayIndex!:number;
+isLibraryEmpty!:boolean;
 areDeleteAndUpdateIconsDisabled: boolean=false;
   constructor(private booksService: BooksService) {}
 
   ngOnInit(): void {
     this.books=this.booksService.getBooks();
+    this.isLibraryEmpty=this.books.length==0 ? true : false;
     this.booksService.bookAdded.subscribe(
       (updatedBooks: Book[])=>{
         this.books=updatedBooks;
+        this.isLibraryEmpty=false;
+        this.areDeleteAndUpdateIconsDisabled=false;
       }
     );
     this.booksService.isBookUpdatedSuccessfully.subscribe(
@@ -39,6 +43,7 @@ areDeleteAndUpdateIconsDisabled: boolean=false;
   deleteBook(id: number){
     this.booksService.deleteBook(id);
     this.books=this.booksService.getBooks();
+    this.isLibraryEmpty=this.books.length==0 ? true : false;
   }
 
   updateBook(id: number){
